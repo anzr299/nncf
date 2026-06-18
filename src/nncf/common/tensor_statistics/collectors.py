fbe963ada4f42b427135fd744d3c7b01d97b84f2
anzr299 <aamir.nazir@intel.com>
2026-06-18 10:18:27 +0400

    init

--- src/nncf/common/tensor_statistics/collectors.py
+++ src/nncf/common/tensor_statistics/collectors.py
@@ -930,7 +930,9 @@ class HAWQAggregator(AggregatorBase):
         trace = fns.sum(fns.multiply(x, x))
         # NOTE: average trace?? divide by number of diagonal elements
         # TODO(dlyakhov): revise this formula as possibly it is with an error; adopted from previous HAWQ implementation
-        self._container = (self._container + trace) / x.size
+        # We normalize the trace by the number of elements in the tensor so that larger matrices do not dominate
+        # the sensitivity scores
+        self._container += trace / x.size
 
     def _aggregate_impl(self) -> Tensor:
         return self._container * 2 / self._collected_samples
